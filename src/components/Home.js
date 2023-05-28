da5ee4adf0fa70a43a5e5a5fc0ec6c31eb0ab85c
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-05-28 00:21:34 -0700

    added dark-horses pm4, updted css

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Nav from "./Nav";
+// import Horse from "./Horse";
+import myVideo  from "../assets/dark-horse.mp4";
+
+export default function Home({ setScreen }) {
+
+    return (
+        <div className="page-wrapper">
+             <Nav setScreen={setScreen} />
+            <div className="video-background">
+                <video autoPlay loop muted style={{width: "100%"}}>
+                    <source src={myVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+           
+      
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import Nav from "./Nav";
+// import Horse from "./Horse";
+
+
+// export default function Home ({setScreen}){
+
+//     return <div>
+     
+//         <Nav setScreen={setScreen}/>
+//         animated dark horse will go here
+//         <Horse />
+//     </div>
+// }
+
+
